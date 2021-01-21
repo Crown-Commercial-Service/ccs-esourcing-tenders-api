767b28fd58e1af098c2fac3b343d7eb3a6a8a286
Joe Raymond <joe.raymond@roweit.co.uk>
2021-01-21 13:15:13 +0000

    Combine Jaggaer modules into mock Jaggaer server

---
+++ jaggaer-mocksvr-app/src/main/java/uk/gov/crowncommercial/esourcing/JaggaerMockSvrApplication.java
@@ -0,0 +1,12 @@
+package uk.gov.crowncommercial.esourcing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaggaerMockSvrApplication {
+
+  public static void main(final String[] args) {
+    SpringApplication.run(JaggaerMockSvrApplication.class, args);
+  }
+}
