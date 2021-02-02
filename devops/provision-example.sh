950aea999128b01056145bca728bcadc7d89870f
Paul Illingworth <paul.illingworth@roweit.co.uk>
2021-02-02 10:10:35 +0000

    Create bash provisioning example script

--- devops/provision-example.sh
+++ devops/provision-example.sh
@@ -1,4 +1,6 @@
-# Example provisioning script (Windows PowerShell)
+#!/bin/bash
+
+# Example provisioning script (Bash)
 #
 # This is a very simple deployment of a single application with no route or
 # backing services and as such just requires a few environment variables to
@@ -11,7 +13,7 @@
 #
 # As these environment variables need to be set before the application is run
 # and they can't be set until the application is deployed the application must
-# first be deployed but not run. 
+# first be deployed but not run.
 #
 # Before running this scripts ensure the command shell is logged into the
 # cloudfoundry organisation/space with permissions to manage the space.
@@ -22,16 +24,16 @@
 # contain URL "friendly" characters and not contain a '.' as the URL
 # endpoint then looks like a subdomain and the GOV.UK PaaS wildcard HTTPS
 # certificates no longer work.
-$APP_NAME = "sandbox-ccs-esourcing-tenders-api"
+APP_NAME="sandbox-ccs-esourcing-tenders-api"
 
 # push app but do not start
 # cf push --no-start -f .\manifest-sandbox.yml
 
 # optionally set IP addresses if IP restricted
-$SALESFORCE_IP_ADDRESSES = ""
-$JAGGAER_IP_ADDRESSES = ""
-$ROWEIT_IP_ADDRESSES = ""
-$SWAGGER_HUB_IP_ADDRESSES = "3.223.162.99, 18.213.102.186, 23.23.82.88, 34.231.31.110, 54.158.217.73"
+SALESFORCE_IP_ADDRESSES=""
+JAGGAER_IP_ADDRESSES=""
+ROWEIT_IP_ADDRESSES=""
+SWAGGER_HUB_IP_ADDRESSES="3.223.162.99, 18.213.102.186, 23.23.82.88, 34.231.31.110, 54.158.217.73"
 cf set-env $APP_NAME CCS_ESOURCING_IP_ALLOW_LIST "$ROWEIT_IP_ADDRESSES $SWAGGER_HUB_IP_ADDRESSES"
 
 # set the API keys needed to allow access to the application
@@ -50,5 +52,6 @@ cf set-env $APP_NAME CCS_ESOURCING_SALESFORCE_CLIENT_URL ""
 # Application is now deployed but not running. It is anticipated that the actual
 # deployment will be via TravisCI and no other manual configuration is required.
 
-# Start the application (if required)
+# Start or restage the application as required
 # cf start $APP_NAME
+# cf restage test-ccs-esourcing-tenders-api
