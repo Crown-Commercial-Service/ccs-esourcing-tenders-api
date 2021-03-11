b4f78e5b1b64328ae1dcc20d880b9ea64f3eb100
Joe Raymond <joe.raymond@roweit.co.uk>
2021-03-11 15:28:22 +0000

    Change api-key header name back to the default

--- integration-app/src/test/java/uk/gov/crowncommercial/esourcing/integration/api/Constants.java
+++ integration-app/src/test/java/uk/gov/crowncommercial/esourcing/integration/api/Constants.java
@@ -4,7 +4,7 @@ class Constants {
 
   static final String CCS_API_BASE_PATH = "/crown-commercial-service/ccs-esourcing-client/0.0.1-SNAPSHOT";
 
-  static final String API_KEY_HEADER = "api-key";
+  static final String API_KEY_HEADER = "x-api-key";
   
   private Constants() {
   }
